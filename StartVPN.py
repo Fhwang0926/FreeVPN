da1326fac3af23a70ce2c67bc43008a8dcaea4df
Fhwang0926 <hdh0926@naver.com>
2018-07-15 17:10:24 +0900

    demo release just can connect no comment no clean code

--- StartVPN.py
+++ StartVPN.py
@@ -20,21 +20,35 @@ class OpenVPN:
 
     def connect(self):
         try:
+            vpnName = "JustFreeVPN"
             if "win" in sys.platform:
                 print("Disconnecting All VPN's")
                 os.system("rasdial /disconnect")
-                connectcmd = "rasdial "+self.accountInfo['target']+" " + self.accountInfo['id'] + " " + self.accountInfo['pw']+" /phone:"+self.accountInfo['target']
+
+                cmd = 'Add-VpnConnection '
+                cmd += '-Name "'+vpnName+'" '
+                cmd += '-ServerAddress "'+self.accountInfo['host']+'" '
+                cmd += '-TunnelType "PPTP" '
+                cmd += '-EncryptionLevel "Required" '
+                cmd += '-SplitTunneling -AuthenticationMethod MsChapv2 -RememberCredential -Force -PassThru'
+
+                if os.system("rasdial "+vpnName) == 623: os.system("powershell " + cmd)
+                connectcmd = "rasdial "+ vpnName + ' "' + self.accountInfo['id'] + '" "' + self.accountInfo['pw']+'"'
                 os.system(connectcmd)
+                print("[+] JustFreeVPN connected!")
+            if not "win" in sys.platform:
+                print("This OS is Linux, not support")
+
         except Exception as e:
-            print(e)
+            print("connect error : ", e)
 
     def cleaner(self,  data):
         rs = self.reg.findall(data)
+        self.accountInfo['id'] = "pptp"
         for x in rs:
-            if x[0]: self.accountInfo.update({'target' : self.removeTag(x[0])})
-            if x[1]: self.accountInfo.update({'id': self.removeTag(x[1]).split(':')[1].strip()})
+            if x[0]: self.accountInfo.update({'host' : self.removeTag(x[0])})
+            # if x[1]: self.accountInfo.update({'id': self.removeTag(x[1]).split(':')[1].strip()})
             if x[2]: self.accountInfo.update({'pw': self.removeTag(x[2]).split(':')[1].strip()})
-        print(self.accountInfo)
 
     def removeTag(self, x):
         return re.sub(r'<b>|</b>|</li|>|<', '', x)
@@ -42,5 +56,6 @@ class OpenVPN:
 proc = OpenVPN()
 proc.connect()
 
-(Password:.+li)|(Username:/w+<li)|(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)
-
+# reference
+# https://www.tenforums.com/tutorials/90305-set-up-add-vpn-connection-windows-10-a.html
+# https://docs.microsoft.com/en-us/powershell/module/vpnclient/set-vpnconnectiontriggerdnsconfiguration?view=win10-ps
