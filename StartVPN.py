d6e352336bc2f60bd65924a5f9fe69cc4fa0e392
FHWANG\root <hdh0926@naver.com>
2018-07-22 07:52:14 +0900

    change logic

--- StartVPN.py
+++ StartVPN.py
@@ -1,20 +1,22 @@
-import requests, re, os, sys, time
+import requests, re, os, sys, time, uuid
 from bs4 import BeautifulSoup
 # freevpn
 
 class OpenVPN:
 
     def __init__(self):
-        self.accountInfo = {}
-        self.data = []
-        self.reg = re.compile(r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)|(Username:.+li)|(Password:.+li)')
-        self.regUrl = re.compile(r'https?://(www.)?freevpn\..*\.?[a-zA-Z]$')
+        self.account = {}
         self.homepages = []
-        self.getHtml()
+        self.reg = re.compile(r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)|(Username:.+li)|(Password:.+li)')
+        # self.reg = re.compile(r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)|(:<\/b>.+<)|(:<\/b>.+<)')
+        self.regUrl = re.compile(r'https?://(www.)?freevpn\..*\.?[a-zA-Z]$')
+        self.getServerList()
+        self.log = []
+
         pass
 
-    def getHtml(self):    
-        rs = requests.get("https://freevpn.me/accounts/", verify=False)
+    def getServerList(self):
+        rs = requests.get("https://freevpn.se/accounts/", verify=False)
         if rs.status_code != 200: raise ValueError
 
         soup = BeautifulSoup(rs.text, 'html.parser')
@@ -25,64 +27,63 @@ class OpenVPN:
                     self.homepages.append(rs)
             except:
                 pass
-        for url in self.homepages:
-            print("[+] checkVPN : ", url)
-            rs = requests.get(url)
-            if rs.status_code != 200: raise ValueError
-            soup = BeautifulSoup(rs.text, 'html.parser')
-            for x in soup.find_all('li'):
-                if ":" in x: self.getAccountInfo(x)
-            pass
+
+
+    def updateAccount(self, url=''):
+        self.account['host'] = ''
+        self.account['pw'] = ''
+        if url == '': print("ServerList error : is null"); return
+        print("[+] checkVPN : ", url)
+        rs = requests.get(url+"/accounts/", verify=False)
+        if rs.status_code != 200: raise ValueError
+        soup = BeautifulSoup(rs.text, 'html.parser')
+        self.account['id'] = "pptp"
+        for x in soup.find_all('li'):
+            if len(self.account.keys()) == 3: return
+            try:
+                rs = self.reg.search(str(x)).group().split(' ')
+                if len(rs) > 1:
+                    if "Password" in rs[0]: self.account.update({ 'pw' : rs[1].replace("</li", '') })
+                else:
+                    if self.account['host'] =='': self.account.update({'host': rs[0] })
+            except:
+                pass
+            return
 
     def connect(self, vpnName=''):
         try:
-            self.vpnName = vpnName if vpnName else "JustFreeVPN"
-            if "win" in sys.platform:
+            for url in self.homepages:
+                vpnName = vpnName if vpnName else str(uuid.uuid4()).split("-")[4]
+                self.updateAccount(url)
                 print("Disconnecting All VPN's")
-                os.system("rasdial /disconnect")
+                if "win" in sys.platform:
 
-                cmd = 'Add-VpnConnection '
-                cmd += '-Name "'+self.vpnName+'" '
-                cmd += '-ServerAddress "'+self.accountInfo['host']+'" '
-                cmd += '-TunnelType "PPTP" '
-                cmd += '-EncryptionLevel "Required" '
-                cmd += '-SplitTunneling '
-                cmd += '-Force '
-                # cmd += '-RememberCredential' # this option no get gateway from vpn server
-                cmd += '-AuthenticationMethod MsChapv2 '
-                cmd += '-PassThru'
+                    os.system("rasdial /disconnect")
 
-                checkVPN = os.system("rasdial " + vpnName)
-                if checkVPN == 623 or checkVPN ==0: os.system("powershell " + cmd)
-                connectcmd = "rasdial "+ self.vpnName + ' "' + self.accountInfo['host'] + '" "' + self.accountInfo['pw']+'"'
-                print(connectcmd)
-                os.system(connectcmd)
-                print("[+] JustFreeVPN connected!")
-            if not "win" in sys.platform:
-                print("This OS is Linux, not support")
-            # try:
-            #     while 1:    
-            #         os.system("ping 8.8.8.8 -n 1")
-            #         time.sleep(3)
-            # except KeyboardInterrupt:
-            #     self.disconnect()
-            #     self.removeVPN()
+                    cmd = 'Add-VpnConnection '
+                    cmd += '-Name "'+vpnName+'" '
+                    cmd += '-ServerAddress "'+self.account['host']+'" '
+                    cmd += '-TunnelType "PPTP" '
+                    cmd += '-EncryptionLevel "Required" '
+                    cmd += '-SplitTunneling '
+                    cmd += '-Force '
+                    # cmd += '-RememberCredential' # this option no get gateway from vpn server
+                    cmd += '-AuthenticationMethod MsChapv2 '
+                    cmd += '-PassThru'
+
+                    checkVPN = os.system("rasdial " + vpnName)
+                    if checkVPN == 623 or checkVPN ==0: os.system("powershell " + cmd)
+                    connectcmd = "rasdial "+ vpnName + ' "' + self.account['id'] + '" "' + self.account['pw']+'"'
+                    self.log.append(url+" | "+self.account['host']+" | "+connectcmd)
+                    print(url, "|", self.account['host'], " | ", connectcmd)
+                    if os.system(connectcmd) == 807: self.removeVPN(vpnName); print("Re connectting... "); continue
+                    print("connected!")
+                    # try:python3 s
+                if not "win" in sys.platform:
+                    print("This OS is Linux, not support")
+
         except Exception as e:
             print("connect error : ", e)
-
-    def getAccountInfo(self,  data):
-        rs = self.reg.findall(data)
-        print(rs)
-        self.accountInfo['id'] = "pptp"
-        for x in rs:
-            print(rs)
-            if x[0]: self.accountInfo.update({'host' : self.removeTag(x[0])})
-            # if x[1]: self.accountInfo.update({'id': self.removeTag(x[1]).split(':')[1].strip()})
-            if x[2]: self.accountInfo.update({'pw': self.removeTag(x[2]).split(':')[1].strip()})
-        print(self.accountInfo)
-
-    def removeTag(self, x=''):
-        return re.sub(r'<b>|</b>|</li|>|<', '', x)
 
     def disconnect(self, x=''):
         print("rasdial "+( x if x else self.vpnName)+" /disconnect");
@@ -92,14 +93,16 @@ class OpenVPN:
         name = x if x else self.vpnName
         cmd = 'Remove-VpnConnection '
         cmd += '-Force '+name
-
-        if os.system("rasdial "+name) == 623: return os.system("powershell " + cmd)
+        print(os.system("powershell " + cmd))
+        # if os.system("rasdial "+name) == 623: return
         return False
         pass
-
+    def showlog(self):
+        for x in self.log:
+            print(x)
 proc = OpenVPN()
-# proc.connect()
-
+proc.connect()
+proc.showlog()
 # reference
 # https://www.tenforums.com/tutorials/90305-set-up-add-vpn-connection-windows-10-a.html
 # https://docs.microsoft.com/en-us/powershell/module/vpnclient/set-vpnconnectiontriggerdnsconfiguration?view=win10-ps
